1939b03fbe11a84eaabd9edda775c9ac9e6a1ac1
Kamwerudroid <mmakweru@gmail.com>
2025-08-24 19:38:45 +0300

    Basic visual structure for the app

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,7 +3,14 @@ function App() {
   
   return (
     <>
-      <h1 className="text-center">Movie Application</h1>
+    <div className="border-3 m-5 mx-50 rounded bg-[#0f0a0a] text-[#BDBF09] flex-column ">
+      <h1 className="text-center text-3xl font-bold ">Movie Application</h1>
+      <div className="my-5">
+        <p className="text-center text-xl">Movie list</p>
+      </div>
+      
+    </div>
+      
     </>
   )
 }
