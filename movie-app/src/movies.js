92cb891638370cf1bb5c2127b6b36dfe37c94bd5
Kamwerudroid <mmakweru@gmail.com>
2025-08-27 22:31:24 +0300

    movies.js and movielist.jsx added

---
+++ movie-app/src/movies.js
@@ -0,0 +1,37 @@
+export const movies = [
+  {
+    id: 1,
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    year: 2010,
+    imageUrl: 'images/Inception_(2010)_theatrical_poster.jpg'
+  },
+  { 
+    id: 2,
+    title: 'The Matrix',
+    director: 'Lana Wachowski, Lilly Wachowski',
+    year: 1999,
+    imageUrl: 'images/The_Matrix.png'
+  },
+  {
+    id: 3,
+    title: 'Parasite',
+    director: 'Bong Joon Ho',
+    year: 2019,
+    imageUrl: 'images/91EUro56k+L._AC_SL1500_.jpg'
+  },
+  {
+    id: 4,
+    title: 'Interstellar',
+    director: 'Christopher Nolan',
+    year: 2014,
+    imageUrl: 'images/Interstellar_film_poster.jpg'
+  },
+  {
+    id: 5,
+    title: 'Batman Begins',
+    director: 'Christopher Nolan',
+    year: 2005,
+    imageUrl:'images/Batman_Begins_Poster.jpg'
+  }
+];
